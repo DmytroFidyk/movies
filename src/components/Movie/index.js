78c52702aa15d7f879b8a5858aaaba66dfb93f32
Dmytro Fidyk <fidykdl@gmail.com>
2022-05-23 21:00:46 +0300

    Made Favorite button

--- src/components/Movie/index.js
+++ src/components/Movie/index.js
@@ -1,21 +1,32 @@
 import './Movie.css';
+import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
-const Movie = ({ title, posterPath, genres }) => {
+const Movie = ({ movieId, title, posterPath, genres, route }) => {
+
+    const [ isFavorite, changeIsFavorite ] = useState(false);
 
     const path = `https://image.tmdb.org/t/p/w200${posterPath}`;
+
+    let iconPath = '/images/icons/favorite-icon.png';
+
+    if (isFavorite)
+        iconPath = '/images/icons/fill-favorite-icon.png';
 
     return (
         <div className="movie__container">   
             <div className="movie__poster">
                 <img src={ path } alt="Movie poster"/>
-                <div className="overlay">
-                    <h3>Детальніше</h3>
-                </div>
+                
             </div>
             
             <div className="movie__title__container">
-                <h3 className="movie__title">{ title }</h3>    
+                <h3 className="movie__title">{ title }</h3> 
+                <div className="favorite__button round" onClick={() => changeIsFavorite(isFavorite => !isFavorite)}>
+                    <img id="favorite__icon" src={iconPath} alt="Favorite icon"/>
+                </div>   
             </div>
+            
             <div className="movie__genres">Пригоди Фантастика{ /*genres*/ }</div>
         </div>
     );
--- src/components/MovieList/index.js
+++ src/components/MovieList/index.js
@@ -1,30 +1,28 @@
 import './MovieList.css';
 import { useState, useEffect } from 'react';
 import Movie from '../Movie';
-import { Link } from 'react-router-dom';
 
 const MovieList = () => {
     const [ movies, setMovies ] = useState([]);
 
-  useEffect(() => {
-    fetch('https://api.themoviedb.org/3/movie/popular?api_key=d7de2b3fba336e7ceb28c02600603538')
-      .then(response => response.json())
-      .then(json => {
-        console.log(json.results);
-        setMovies(json.results);
-      });
-  }, []);
+    useEffect(() => {
+        fetch('https://api.themoviedb.org/3/movie/popular?api_key=d7de2b3fba336e7ceb28c02600603538')
+        .then(response => response.json())
+        .then(json => {
+            console.log(json.results);
+            setMovies(json.results);
+        });
+    }, []);
 
-  const moviesList = movies.map(movie => {
-     return (
-            <Link key={movie.id} to="/details" state={{ movieId: movie.id }} className="link">
-                <Movie 
-                    key={movie.id} 
-                    title={movie.title} 
-                    posterPath={movie.poster_path} 
-                    genres={movie.genre_ids}
-                />
-            </Link>
+    const moviesList = movies.map(movie => {
+        return (
+            <Movie 
+                key={movie.id}
+                movieId={movie.id} 
+                title={movie.title} 
+                posterPath={movie.poster_path} 
+                genres={movie.genre_ids}
+            />
         );
     });
 

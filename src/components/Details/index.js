c84cac5fb4ed9ea0037981699eaf4a65e76a8c01
Dmytro Fidyk <fidykdl@gmail.com>
2022-05-29 18:29:53 +0300

    Made feature adding to favorites and remove from it at details page

--- src/components/Details/index.js
+++ src/components/Details/index.js
@@ -21,6 +21,27 @@ const Details = ({ allGenres, addToFavorites, removeFromFavorites}) => {
             });
     }, [movieId]);
 
+    const [ isFavorite, changeIsFavorite ] = useState(false);
+
+    useEffect(() => {      
+        const favorites = JSON.parse(localStorage.getItem('favorites'));
+        
+        if (favorites.length > 0) {
+            if (favorites.some(movie => movie.id === movieId)) {               
+                changeIsFavorite(isFavorite => !isFavorite);
+            } 
+        }              
+    }, []);
+
+    let iconPath = '/images/icons/favorite-icon.png';
+
+    if (isFavorite) {
+        iconPath = '/images/icons/fill-favorite-icon.png';
+    } else {
+        iconPath = '/images/icons/favorite-icon.png';
+    }
+
+
     const path = `https://image.tmdb.org/t/p/w400${movie.poster_path}`;
 
     let genres;
@@ -29,6 +50,22 @@ const Details = ({ allGenres, addToFavorites, removeFromFavorites}) => {
         genres = movie.genres.map(genre => <div key={genre.id} className="genre">{genre.name}</div>);
     }
 
+    let languages;
+
+    if (movie.spoken_languages) {
+        languages = movie.spoken_languages.map(language => {
+            return <span key={language.name} className="language">{language.english_name}</span>;
+        });
+    }
+
+    let countries;
+
+    if (movie.production_countries) {
+        countries = movie.production_countries.map(country => {
+            return <span key={country.name} className="country">{country.name}</span>;
+        });
+    }
+     
     return (
         <>
             <div className="details__container">
@@ -36,14 +73,48 @@ const Details = ({ allGenres, addToFavorites, removeFromFavorites}) => {
                     { movie.poster_path && <img id="details__poster" src={path} alt="Movie poster"/> }
                 </div>
                 <div className="text__container">
-                    <h2>{ movie.title + ` (${ movie.release_date ? movie.release_date.slice(0, 4) : ''})`}</h2>
+                    <div className="title__container details__title">
+                        <h2>{ movie.title + ` (${ movie.release_date ? movie.release_date.slice(0, 4) : ''})`}</h2>
+                        <div className="favorite__button" onClick={() => {
+                            if (isFavorite === false) {   
+                                addToFavorites(movie);    
+                            }
+                            else {
+                                removeFromFavorites(movie);
+                            }         
+                            changeIsFavorite(isFavorite => !isFavorite);           
+                        }}> 
+                            <img id="favorite__icon__details" src={iconPath} alt="Favorite icon"/>
+                        </div>
+                    </div>
+                    
                     <div className="genres__container">
                         { genres }
                     </div>                    
-                    <p>{ movie.overview }</p>
+                    <div><p>{ movie.overview }</p></div>
+                    <div className="status__container">
+                       Status: {movie.status}
+                    </div>
+                    <div className="languages__container">
+                       Spoken languages: {languages}
+                    </div>
+                    <div className="countries__container">
+                       Production countries: {countries}
+                    </div>
+                    <div className="popularity__container">
+                       Popularity: {movie.popularity}
+                    </div>
+                    <div className="votes__container">
+                       Vote average: {movie.vote_average}
+                    </div>
                 </div>
             </div>
-            <Recommendations movieId={movieId} allGenres={allGenres} addToFavorites={addToFavorites} removeFromFavorites={removeFromFavorites} />
+            <Recommendations 
+                movieId={movieId} 
+                allGenres={allGenres} 
+                addToFavorites={addToFavorites} 
+                removeFromFavorites={removeFromFavorites} 
+            />
         </>
     );
 };
--- src/components/Favorites/index.js
+++ src/components/Favorites/index.js
@@ -4,10 +4,9 @@ import { useState, useEffect } from 'react';
 import Movie from '../Movie';
 
 const Favorites = ({favorites, addToFavorites, removeFromFavorites}) => {
-   
-
+    console.log('Фаворити:', favorites);
     const favoritesMovies = favorites.map(movie => {
-        return <Movie key={movie.id} movieId={movie.id} title={movie.title} posterPath={movie.poster} favorite={movie.isFavorite} addToFavorites={addToFavorites} removeFromFavorites={removeFromFavorites}/>
+        return <Movie key={movie.id} movieId={movie.id} title={movie.title} posterPath={movie.poster || movie.poster_path} favorite={movie.isFavorite} addToFavorites={addToFavorites} removeFromFavorites={removeFromFavorites}/>
     });
 
     return (
